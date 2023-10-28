461fea8943ea019fd41cc91d0a27e12da18d2fca
yeseong31 <yeseong31@naver.com>
2023-10-28 22:34:23 +0900

    refactor(PlayerService): Pattern Matching switch문 간소화

--- src/main/java/baseball/service/PlayerService.java
+++ src/main/java/baseball/service/PlayerService.java
@@ -8,14 +8,10 @@ public class PlayerService {
     private static final String WRONG_CHOICE_MESSAGE = "잘못된 선택입니다.";
 
     public boolean selectOption(String optionNumber) {
-        switch (Integer.parseInt(optionNumber)) {
-            case RESTART_OPTION -> {
-                return true;
-            }
-            case TURN_OFF_OPTION -> {
-                return false;
-            }
+        return switch (Integer.parseInt(optionNumber)) {
+            case RESTART_OPTION -> true;
+            case TURN_OFF_OPTION -> false;
             default -> throw new IllegalArgumentException(WRONG_CHOICE_MESSAGE);
-        }
+        };
     }
 }
